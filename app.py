f8efc2881c1db00c459271d6f61e501931797642
Udaya Gollapalli <udaya.gollapalli@matthews.se>
2025-05-03 23:01:48 +0200

    fix app.py file

--- app.py
+++ app.py
@@ -13,4 +13,4 @@ def index():
     return Time+" Serving from "+h_name+" ("+IP_addres+")\n"
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=80)
+    app.run(host='0.0.0.0', port=5000)
